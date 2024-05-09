f61c8fcc6ba460377ab7226b8cfe8f5da08d15e0
dp <nathiprasad2404@gmail.com>
2024-05-09 12:23:53 +0530

    logout

--- src/Components/Logout.jsx
+++ src/Components/Logout.jsx
@@ -1,33 +1,38 @@
 import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import URL_FOR_API from '../API/UrlOfApi';
+import Cookies from 'js-cookie'
 
 function Logout() {
   const history = useNavigate();
+  const logoutUser = async () =>{
+    Cookies.remove("jwtoken")
+    history('/')
+  }
 
   useEffect(() => {
-    const logoutUser = async () => {
-      try {
-        const res = await fetch(URL_FOR_API + "/api/logout", {
-          method: "GET",
-          headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json"
-          },
-          credentials: "include"
-        });
+    // const logoutUser = async () => {
+    //   try {
+    //     const res = await fetch(URL_FOR_API + "/api/logout", {
+    //       method: "GET",
+    //       headers: {
+    //         Authorization: `Bearer ${jwtToken}`,
+    //         Accept: "application/json",
+    //         "Content-Type": "application/json"
+    //       },
+    //       credentials: "include"
+    //     });
 
-        if (res.status !== 200) {
-          throw new Error('Logout failed');
-        }
+    //     if (res.status !== 200) {
+    //       throw new Error('Logout failed');
+    //     }
 
-        // Logout successful, redirect to home page
-        history('/');
-      } catch (err) {
-        console.error(err);
-        // Handle error, maybe display a message to the user
-      }
-    };
+    //     // Logout successful, redirect to home page
+    //     history('/');
+    //   } catch (err) {
+    //     console.error(err);
+    //     // Handle error, maybe display a message to the user
+    //   }
+    // };
 
     logoutUser();
     // eslint-disable-next-line react-hooks/exhaustive-deps
